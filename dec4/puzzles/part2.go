727eb816707cc28e9fe9f61bae1194328d33d213
jre4851 <jre4851@gmail.com>
2025-12-04 21:45:24 -0600

    Implements part 2 solution for puzzle
    Solves the puzzle by iteratively optimizing the floor plan based on neighbor counts, reducing the number of occupied seats until stability.

--- dec4/puzzles/part2.go
+++ dec4/puzzles/part2.go
@@ -1,10 +1,46 @@
 package puzzles
 
 import (
-    "log"
-    //"dec4/helpers"
+	"dec4/helpers"
+	"log"
 )
 
 func Part2() {
-    log.Println("Part 2 placeholder")
+	floorPlan, err := helpers.ValidateInput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	neighbors := [][2]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1},          {0, 1},
+		{1, -1},  {1, 0},  {1, 1},
+	}
+
+	var optimizedCount int
+	var moarPaper bool = true
+	for moarPaper {
+		moarPaper = false
+		for i, current := range floorPlan {
+			if current.Value == "@" {
+				atCount := 0
+				for _, n := range neighbors {
+					nx, ny := current.PositionX+n[0], current.PositionY+n[1]
+					for _, check := range floorPlan {
+						if check.PositionX == nx && check.PositionY == ny && check.Value == "@" {
+							atCount++
+							break
+						}
+					}				
+				}
+				if atCount < 4 {						
+						floorPlan[i].Value = "."
+						moarPaper = true
+						optimizedCount++
+				}
+			}
+		}
+	}
+
+	log.Printf("Part 2 - Optimized Rolls: %d", optimizedCount)
 }
