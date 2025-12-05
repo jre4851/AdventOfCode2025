bf9541b95a6f4aa189c731f43c37d7f48fef1394
jre4851 <jre4851@gmail.com>
2025-12-04 21:24:11 -0600

    Implements paper roll optimization logic
    Adds functionality to read paper roll data from a file, parse it, and determine which rolls can be optimized based on their neighbors.
    
    This enhances performance by identifying and processing only the necessary paper rolls.

--- dec4/helpers/validateInput.go
+++ dec4/helpers/validateInput.go
@@ -1,5 +1,44 @@
 package helpers
 
-func ValidateInput() {
-    // TODO: Implement input validation
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type PaperRoll struct {
+	PositionX int
+	PositionY int
+	Value string
+}
+
+func ValidateInput() ([]PaperRoll, error) {
+    file, err := os.Open("./input/aocInput.txt")
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return nil, err
+		}
+		defer file.Close()
+
+		var floorPlan []PaperRoll
+		scanner := bufio.NewScanner(file)
+		yIndex := 0
+		for scanner.Scan() {
+			line := scanner.Text()
+			for xIndex, char := range line {
+				floorPlan = append(floorPlan, PaperRoll{
+					PositionX: xIndex,
+					PositionY: yIndex,
+					Value: string(char),
+				})
+			}
+			yIndex++
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading file:", err)
+			return nil, err
+		}
+
+		return floorPlan, nil
 }
--- dec4/puzzles/part1.go
+++ dec4/puzzles/part1.go
@@ -1,10 +1,40 @@
 package puzzles
 
 import (
-    "log"
-    //"dec4/helpers"
+	"dec4/helpers"
+	"log"
 )
 
 func Part1() {
-    log.Println("Part 1 placeholder")
+	floorPlan, err := helpers.ValidateInput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	neighbors := [][2]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1},          {0, 1},
+		{1, -1},  {1, 0},  {1, 1},
+	}
+
+	var optimizedRolls []helpers.PaperRoll
+	for _, current := range floorPlan {
+		if current.Value == "@" {
+			atCount := 0
+			for _, n := range neighbors {
+				nx, ny := current.PositionX+n[0], current.PositionY+n[1]
+				for _, check := range floorPlan {
+					if check.PositionX == nx && check.PositionY == ny && check.Value == "@" {
+						atCount++
+						break
+					}
+				}				
+			}
+			if (atCount < 4)  {
+					optimizedRolls = append(optimizedRolls, current)
+				}
+		}
+	}
+
+	log.Println("Part 1 - Optimized Rolls:", len(optimizedRolls))
 }
