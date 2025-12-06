8b18c8f63043e4e517ef6bd9776c1b14e570f8d8
jre4851 <jre4851@gmail.com>
2025-12-06 15:31:41 -0600

    Improves Part 2 solution with a greedy algorithm
    Refactors Part 2 to use a more efficient greedy algorithm for finding the largest possible number from the input string.
    
    This change replaces the previous sorting-based approach with an algorithm that iterates through the string and picks the largest digit at each step, optimizing for performance and reducing memory usage.
    Uses big.Int to handle large numbers.

--- dec3/puzzles/part2.go
+++ dec3/puzzles/part2.go
@@ -3,8 +3,7 @@ package puzzles
 import (
 	"dec3/helpers"
 	"log"
-	"sort"
-	"strconv"
+	"math/big"
 )
 
 func Part2() {
@@ -13,31 +12,33 @@ func Part2() {
         log.Fatal(err)
     }
 
-    grandTotal := 0
+    grandTotal := big.NewInt(0)
     for _, inputStr := range inputs {
-        digits := make([]int, len(inputStr))
-        for i, ch := range inputStr {
-            digits[i] = int(ch - '0')
+        k := 12
+        n := len(inputStr)
+        if n < k {
+            k = n
         }
-        sort.Slice(digits, func(i, j int) bool {
-            return digits[i] > digits[j]
-        })
-        // Take up to 12 largest digits
-        n := 12
-        if len(digits) < 12 {
-            n = len(digits)
+        // Greedy algorithm: build the largest number by picking k digits in order
+        result := make([]byte, 0, k)
+        start := 0
+        for i := 0; i < k; i++ {
+            maxDigit := byte('0')
+            maxPos := start
+            // The range to search is from start to n - (k - i)
+            end := n - (k - i)
+            for j := start; j <= end; j++ {
+                if inputStr[j] > maxDigit {
+                    maxDigit = inputStr[j]
+                    maxPos = j
+                }
+            }
+            result = append(result, maxDigit)
+            start = maxPos + 1
         }
-        largestDigits := digits[:n]
-        // Concatenate digits into a string
-        digitStr := ""
-        for _, d := range largestDigits {
-            digitStr += strconv.Itoa(d)
-        }
-        totalSum, err := strconv.Atoi(digitStr)
-        if err != nil {
-            log.Fatal(err)
-        }
-        grandTotal += totalSum
+        bankJoltage := big.NewInt(0)
+        bankJoltage.SetString(string(result), 10)
+        grandTotal.Add(grandTotal, bankJoltage)
     }
-    log.Printf("Part 2 Total: %d", grandTotal)
-}
+    log.Printf("Part 2 Total: %s", grandTotal.String())
+	}
